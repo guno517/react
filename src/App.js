7ed9a95b9d072b6701c4883d324161e68c8f449d
guno517 <60571418+guno517@users.noreply.github.com>
2021-02-13 19:23:47 +0900

    Array, CRUD, spread, concat, filter, map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import Hello from './hello';
 import './App.css';
 import Wrapper from './wrapper';
 import Counter from './Counter';
 import InputSample from './inputSample';
+import UserList from './UserList'
+import CreateUser from './CreateUser';
 
 function App() {  
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:''
+  })
+
+  const {username, email} = inputs;
+  const onChange = (e) => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+  const [users,setUsers] = useState([
+    {
+        id:1,
+        username: 'guno',
+        email: 'guno@google.com',
+        active:true
+    },
+    {
+        id:2,
+        username: 'test',
+        email: 'test@google.com',
+        active:false
+    },
+    {
+        id:3,
+        username: 'bgo',
+        email: 'bgo@google.com',
+        active:false
+    },
+  ]);
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    }
+    setUsers([...users, user]) //기존의 배열을 복사하고 새로운 배열을 만들고 user을 넣는다
+    //setUsers(users.concat(user));
+    //user.push(user) 는 업데이트가 되지 않는다. XXXXX
+    setInputs({
+      username:'',
+      email:'',
+    });
+    console.log(nextId.current); // 4
+    nextId.current += 1;
+  }
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id)); //id를 비교한다
+  }
+
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id ===id //user을 파라미터로 가져온다
+        ? {...user, active: !user.active }
+        : user
+    ));
+  }
+
   return (
     <>
     <Wrapper>
@@ -15,6 +81,11 @@ function App() {
     <Counter />
     <hr />
     <InputSample />
+    <hr />
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    </>
     </>
   );
 }
---
+++ src/UserList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function User({user, onRemove, onToggle}){
+    const {username, email, id, active} = user; // {user.username}, {user.email}, {user.id}를 <div>내에서 쓰기 귀찮다면
+    return(
+        <div>
+            <b style={{
+                color: active? 'green': 'black',
+                cursor: 'pointer'
+            }} 
+                onClick={() => onToggle(id)}
+            >
+            {username}</b> <span>{email}</span>
+            <button onClick={() => onRemove(id)}>삭제</button>  {/**함수를 호출하는게 아니라 새로 만들어 넣어야한다. ()=>없이 onClick={onRemove(id)}만 있으면 렌더링 될때마다 값이 삭제되어있다. */}
+            {/**버튼을 클릭하면 이러한 함수를 호출할 것이다 id값을 파라미터로 넣어서 호출할것이다 */}
+            {/**값을 받아 값에 해당하는 데이터를 지워야하기 때문에 파라미터를 받기 위해 새로운 익명 함수를 호출 */}
+        </div>
+    );
+}
+
+function UserList({users, onRemove, onToggle}) {
+    return (
+        <div>
+            {
+                users.map(
+                    user => (
+                    <User 
+                        user={user}
+                        key={user.id} //key가 없으면 웹의 효율성이 떨어진다. 모든 정보가 다시 업데이트된다(?)
+                        // 새로운 항목이 추가되거나 삭제될 때, 효율적으로 관리할 수 있다.
+                        onRemove={onRemove}
+                        onToggle={onToggle}
+                    />) 
+                )
+            }
+        </div>
+    );
+}
+
+export default UserList;
