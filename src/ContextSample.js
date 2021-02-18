d2d0f651db108174f695fe7f2de5c8f633f78327
guno517 <60571418+guno517@users.noreply.github.com>
2021-02-18 17:02:02 +0900

    useState사용

--- src/ContextSample.js
+++ src/ContextSample.js
@@ -1,6 +1,6 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext, useContext, useState} from 'react';
 
-const MyContext = createContext('defaultValue');
+const MyContext = createContext('defaultValue'); // <MyContext />가 사용되지 않았을 때의 기본 값.
 
 function Child(){
     const text = useContext(MyContext); // context에 있는 값을 읽어와서 사용할 수 있도록 하는 hook
@@ -16,9 +16,11 @@ function GrandParent(){
 }
 
 function ContextSample(){
+    const [value, setValue] = useState(true);
     return (
-    <MyContext.Provider value="GOOD"> {/**MyContext.Provider를 통해서 value값을 설정을 하면 createContext값이 value값으로 변환 */}
+    <MyContext.Provider value={value ? "GOOD" : 'BAD'}> {/**MyContext.Provider를 통해서 context(value)값을 설정을 하면 createContext값이 value값으로 변환 */}
         <GrandParent />
+        <button onClick={() => setValue(!value)}>Click Me!</button> {/**value가 true면 false로 전환 */}
     </MyContext.Provider>
     )
 }
