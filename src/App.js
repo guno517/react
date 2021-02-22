881d49bc50b04570a2110cbe149fc4072a920c66
guno517 <60571418+guno517@users.noreply.github.com>
2021-02-22 14:00:40 +0900

    prettier

--- src/App.js
+++ src/App.js
@@ -1,73 +1,75 @@
-import React, { useReducer , useRef, useMemo, useCallback, createContext} from 'react';
+import React, { useReducer, useMemo, createContext } from 'react';
 import './App.css';
-import UserList from './UserList'
+import UserList from './UserList';
 import CreateUser from './CreateUser';
 import useInputs from './useInputs';
+import Hello from './Hello';
 
-function countActiveUsers(users){
+function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active === true).length;
+  return users.filter((user) => user.active === true).length;
 }
 
 const initialState = {
-  users:[
+  users: [
     {
-        id:1,
-        username: 'guno',
-        email: 'guno@google.com',
-        active:true
+      id: 1,
+      username: 'guno',
+      email: 'guno@google.com',
+      active: true,
     },
     {
-        id:2,
-        username: 'test',
-        email: 'test@google.com',
-        active:false
+      id: 2,
+      username: 'test',
+      email: 'test@google.com',
+      active: false,
     },
     {
-        id:3,
-        username: 'bgo',
-        email: 'bgo@google.com',
-        active:false
+      id: 3,
+      username: 'bgo',
+      email: 'bgo@google.com',
+      active: false,
     },
-  ]
-}
+  ],
+};
 
-function reducer(state, action){
-  switch(action.type){
-      case 'CREATE_USER':
-        return {
-          inputs: initialState.inputs,
-          users: state.users.concat(action.user)          
-        };
-      case 'TOGGLE_USER':
-        return {
-          ...state,
-          users: state.users.map(user => 
-              user.id === action.id ? {...user, active: !user.active} : user
-          )
-        };
-      case 'REMOVE_USER':
-        return {
-          ...state,
-          users: state.users.filter(user => user.id !== action.id)
-        }
-      default:
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user,
+        ),
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
       throw new Error('Unhandled');
   }
 }
 
 export const UserDispatch = createContext(null);
 
-function App() {  
+function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const {users} = state; 
+  const { users } = state;
 
-  const count = useMemo(() => countActiveUsers(users), [users])
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <UserDispatch.Provider value={dispatch}>
+      <Hello color="red" name="guno" isSpecial={true} />
       <CreateUser />
-      <UserList users={users}/>
+      <UserList users={users} />
       <div>활성사용자 수 : {count}</div>
     </UserDispatch.Provider>
   );
--- src/hello.js
+++ src/hello.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Hello({color, name, isSpecial}){
-    return (<div style={{
-        color
-    }}>
-        {isSpecial && <b>*</b>}
-        Hello {name}</div>);
+function Hello() {
+  const [value, setValue] = useState('');
+  const onChange = (e) => {
+    const text = e.target;
+    setValue(text.value);
+  };
+  return (
+    <div>
+      <input onChange={onChange} placeholder="값 입력" value={value} />
+      <button>제출</button>
+      <div> 값: {value}</div>
+    </div>
+  );
 }
-
-Hello.defaultProps ={
-    name: '이름없음'
-}
-
 export default Hello;
